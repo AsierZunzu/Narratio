8b0ce09e3cdc12b393000c631965e96a2ba2d631
Asier Zunzunegui <asier.zunzu@gmail.com>
2026-03-15 16:18:29 +0100

    Update linter

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+);
